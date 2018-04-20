7d390f03a87fdd7a9af6126f55190ab59afa1d4c
younggeeks <younggeeks101@gmail.com>
2018-04-21 00:55:06 +0300

    [Feature] add middleware functionalities

--- Sources/KituraLoggerMiddleware/KituraLoggerMiddleware.swift
+++ Sources/KituraLoggerMiddleware/KituraLoggerMiddleware.swift
@@ -1,3 +1,18 @@
-struct KituraLoggerMiddleware {
-    var text = "Hello, World!"
+import Kitura
+import Foundation
+
+public class LoggerMiddleware:RouterMiddleware{
+    public func handle(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws {
+        print("Hello Visitor \n")
+        print("You are from \(request.hostname):\(request.port)")
+        print("Interestingly You used \(request.method)")
+        print("Your User Agent is \(String(describing: request.headers["User-Agent"]))")
+        
+        next()
+    }
+    
+    public init(){
+        
+    }
+    
 }
